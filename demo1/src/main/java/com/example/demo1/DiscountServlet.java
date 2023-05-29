c732724a22a691da69cac2e39824c26c3b8b437c
võ văn tín <vovantin9502@gmail.com>
2023-05-29 17:08:21 +0700

    first commit

---
+++ demo1/src/main/java/com/example/demo1/DiscountServlet.java
@@ -0,0 +1,28 @@
+package com.example.demo1;
+
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "DiscountServlet", value = "/DiscountServlet")
+public class DiscountServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       request.getRequestDispatcher("/bai_9/bai_9.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        double price = Double.parseDouble(request.getParameter("price"));
+        double discountPercent = Double.parseDouble(request.getParameter("discount_percent"));
+        double result = price * discountPercent * 0.01;
+        request.setAttribute("discountPrice",result);
+         request.getRequestDispatcher("/bai_9/result.jsp").forward(request,response);
+    }
+}
---
+++ demo1/src/main/webapp/bai_9/bai_9.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 5/29/2023
+  Time: 1:44 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form method="post" action="/DiscountServlet">Product Discount Calculator
+    <br>
+    <br>
+    <label for="description">Product Description</label>
+    <input type="text" name="description" id="description">
+
+    <label for="price">List Price</label>
+    <input type="number" name="price" id="price">
+
+    <label for="discount_percent">Discount Percent</label>
+    <input type="number" name="discount_percent" id="discount_percent">
+    <br>
+    <button type="submit" value="result">
+        Calculate Discount
+    </button>
+</form>
+</body>
+</html>
