67330889e341525dc8a7d13cd16f243d4250a37a
võ văn tín <vovantin9502@gmail.com>
2023-06-01 16:40:30 +0700

    first commit

--- bai_12/thuc_hanh/src/main/java/com/example/thuc_hanh/service/impl/UserDAOService.java
+++ bai_12/thuc_hanh/src/main/java/com/example/thuc_hanh/service/impl/UserDAOService.java
@@ -48,13 +48,13 @@ public class UserDAOService implements IUserDAOService {
             preparedStatement.setInt(1, id);
             System.out.println(preparedStatement);
             // Step 3: Execute the query or update query
-            ResultSet rs = preparedStatement.executeQuery();
+            ResultSet resultSet = preparedStatement.executeQuery();
 
             // Step 4: Process the ResultSet object.
-            while (rs.next()) {
-                String name = rs.getString("name");
-                String email = rs.getString("email");
-                String country = rs.getString("country");
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String country = resultSet.getString("country");
                 user = new User(id, name, email, country);
             }
         } catch (SQLException e) {
@@ -74,14 +74,14 @@ public class UserDAOService implements IUserDAOService {
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);) {
             System.out.println(preparedStatement);
             // Step 3: Execute the query or update query
-            ResultSet rs = preparedStatement.executeQuery();
+            ResultSet resultSet = preparedStatement.executeQuery();
 
             // Step 4: Process the ResultSet object.
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String email = rs.getString("email");
-                String country = rs.getString("country");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String country = resultSet.getString("country");
                 users.add(new User(id, name, email, country));
             }
         } catch (SQLException e) {
